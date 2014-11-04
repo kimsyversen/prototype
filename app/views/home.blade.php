38048f446428241a3746ac32d931976e42dfc676
Kim Syversen <kim@kImac.local>
2014-11-04 22:08:39 +0100

    Saving work

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -6,10 +6,14 @@
     <body>
         @include('includes.topmenu')
 
-        @yield('body')
-
         {{HTML::script('http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js')}}
         {{HTML::script('http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js')}}
         {{HTML::script('js/slidein-menu.js')}}
+
+        @yield('body')
+
+        <div class="well">
+        <p>Test</p>
+        </div>
     </body>
 </html>
