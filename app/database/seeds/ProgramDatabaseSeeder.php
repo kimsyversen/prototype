3beffc99f0c4f1508bd66b494518dca7a58adca6
Kim Syversen <kim.syversen@gmail.com>
2014-11-13 15:01:15 +0100

    Add more test data

--- app/database/seeds/ProgramDatabaseSeeder.php
+++ app/database/seeds/ProgramDatabaseSeeder.php
@@ -326,6 +326,335 @@ Resultatene fra workshopen er tenkt brukt senere i konferansen. Vel møtt til en
             'type' => 1,
         ));
 
+        Program::create(array(
+            'title' => 'Registrering',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '08:30',
+            'time_to' => '09:30',
+            'description' => 'Registrer deg ved resepsjonen ved ankomst.',
+            'location' => 'Resepsjonen',
+            'speaker' => 'null',
+            'confirmed' => true,
+            'type' => 1,
+        ));
+
+        Program::create(array(
+            'title' => 'Åpning',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '09:30',
+            'time_to' => '11:00',
+            'description' => 'Prorektor Berit Kjeldstad ønsker velkommen og åpner konferansen.',
+            'location' => 'Cosmos 1',
+            'speaker' => 'Prorektor Berit Kjeldstad',
+            'confirmed' => true,
+            'type' => 1,
+        ));
+
+        Program::create(array(
+            'title' => 'Halveis til framtiden - digitale trender og impulser',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '09:30',
+            'time_to' => '11:00',
+            'description' => 'Truls har skrevet en bok sammen med Adjiedj Bakaas med samme tittel som foredraget. Boka peker på 5 megatrender og gir en rekke innspill på digitale trender og impulser vil endre det meste.',
+            'location' => 'Cosmos 1',
+            'speaker' => 'Truls Berg, grunder og innovatør',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'Digital kommunikasjon',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '09:30',
+            'time_to' => '11:00',
+            'description' => 'Digital kommunikasjon skal være hovedregelen i dialogen mellom offentlig sektor og innbyggerne. Vi hører om de nye digitale byggeklossene, SvarUt, kontakt- og reservasjonsregisteret og digital post til innbyggere. Disse skal inngå i den digitale grunnmuren. Hva betyr dette egentlig for innbyggerne, kommunene og statlige virksomheter? Er vi i mål med grunnmuren når disse kommer på plass? Og hvilken utvikling kan vi forvente etter dette? I samtalen vil de fire reflektere blant annet rundt disse spørsmålene, og komme inn på: Betydningen av samhandling og samarbeid mellom kommune og stat; Veikart for felleskomponenter for å sikre forutsigbarhet, fremdrift og en helhetlig tilnærming; Digital kompetanse hos ledere.',
+            'location' => 'Cosmos 1',
+            'speaker' => 'Tone Bringedal, DIFI og Trude Andresen, KS i samtale med Kjetil Århus, Bergen kommune og Ingunn Cowan, Lånekassen',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'Presentasjon av kandidater til Fyrlykt-prisen',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '09:30',
+            'time_to' => '11:00',
+            'description' => 'Presentasjon av kandidater til Fyrlykt-prisen.',
+            'location' => 'Cosmos 1',
+            'speaker' => 'Prorektor Berit Kjeldstad',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'Kaffepause. Spekemat, ost og kjeks',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:00',
+            'time_to' => '11:15',
+            'description' => 'Det blir servert kaffe og spekemat med ost og kjeks i fellesområdet.',
+            'location' => 'Fellesområdet',
+            'speaker' => 'null',
+            'confirmed' => true,
+            'type' => 1,
+        ));
+
+        Program::create(array(
+            'title' => 'U1 - Software Innovation: Skyløsninger',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:15',
+            'time_to' => '11:45',
+            'description' => 'Seminaret inneholder litt om hva cloud er, bakgrunn, skytjeneste vs. Lokal installasjon, gevinster og en rask demonstrasjon av løsningen.',
+            'location' => 'Cosmos 3C',
+            'speaker' => 'Odd-Henrik Hansen og Tonje Nordli. ',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'U2 - EVRY: Mobile trender og løsninger for offentlig sektor',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:15',
+            'time_to' => '11:45',
+            'description' => 'Bruken av mobil og nettbrett øker i bedriftene, og i 2015 vil det være flere på arbeidsplassen som bruker internett gjennom mobile enheter enn gjennom ordinær PC. Antall app’er skyter i været, både bedrifts-apper og apper for privat bruk. Er sikkerheten ivaretatt? Hvordan kan EVRY gi den offentlige sektor merverdi som gjør den mobile enheten til et effektivt arbeidsverktøy? Vi vil også presentere hvordan EVRY kan levere hele livssyklusen innen mobile løsninger, fra mobiltelefoner og nettbrett samt logistikk rundt dette, til sikkerhetsløsninger, apphåndtering og kundeunike apper.
+
+Vi har med oss Kultur- og naturreise, et prosjekt under Kartverket, Kulturrådet, Miljødirektoratet, Riksantikvaren og Riksarkivet. De vil presentere KNappen, som står for kultur- og naturreise-appen og omtales på deres nettsider http://kulturognaturreise.no. Denne appen er en demonstrator på hva et nasjonalt løft for å øke tilgangen til åpne offentlig data og lokal kunnskap om kultur og natur kan føre til. Utrustet med smarttelefon får du tilgang til aktuelle fakta og fortellinger om natur og kultur på stedet de er. Det overordnede målet er å øke kunnskap om og skape engasjement for kulturminner og naturverdier. KNappen er utviklet i samarbeid med EVRY, og kan testes her: http://kulturognaturreise.wordpress.com/2013/04/23/vil-du-vaere-med-a-teste-knappen/',
+            'location' => 'Space 3',
+            'speaker' => 'Anita Rusten, EVRY og Sidsel Hindal og Marit Kjeksrud Amundsen, Kultur- og naturreise ',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'U3 - Brønnøysundregistrene: Med Altinn i fremtiden',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:15',
+            'time_to' => '11:45',
+            'description' => 'Ingen diskripsjon',
+            'location' => 'Cosmos 1',
+            'speaker' => 'Direktør Lars Peder Brekk, Brønnøysundregistrene ',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'U4 - Capgemini: Kunsten å lage verdiskapende, brukersentrerte digitale tjenester',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:15',
+            'time_to' => '11:45',
+            'description' => 'De offentlige tjenestene blir mer og mer digitale, og epoken hvor fysiske kontaktpunkter var de viktigste for å få tilgang til disse er ved veis ende. Sektorer slik som bank og finans har allerede opplevd paradigmeskiftet fra å ha fysiske tjenester til å bli så og si heldigitale.
+
+Fra det offentlige, er Lånekassen et godt eksempel på hvordan det er mulig å modernisere og digitalisere sine tjenester ved å ha et brukersentrisk fokus og samtidig få en effektivisering og kostnadsbesparing internt.
+
+De digitale tjenestene gir med andre ord uante muligheter for både tilbyder og bruker - og med digitalt førstevalg fra 2016, vil antall tjenester som må forholde seg til digitale kontaktpunkter økes dramatisk!
+
+Men hvordan går man frem for å danne tjenester som bruker de digitale kontaktflatene på best mulig måte?',
+            'location' => 'Cosmos 3A',
+            'speaker' => 'Israr Khan',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'U5 - Telenor: Velferdsteknologi i praksis',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:15',
+            'time_to' => '11:45',
+            'description' => 'Værnes-regionen var tidlig ute med bruk av digitale løsninger innenfor helse og omsorg. Stjørdal og Selbu var de første kommunene som innførte løsning der velferdsteknologi ble integrert med fagsystemet og journalsystemet i kommunen. Hva er utfordringene med helse og omsorg i kommunene og hva er målet med innføring av ny teknologi? Vi får en innføring i kommunens erfaringer med løsningen.',
+            'location' => 'Living Room 1 ',
+            'speaker' => 'Elin Darrel, IT-sjef i Stjørdal kommune ',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'U6 - Visma: Hvordan skape innovasjon i din virksomhet?',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:15',
+            'time_to' => '11:45',
+            'description' => 'Vi gir deg en innføring i hvordan du kan skape en innovasjonskultur i din organisasjon. Ragne forteller om hva vi i Visma gjør for å fange, foredle og implementere nye idèer fra egen organisasjon i forretningsutviklingen, og gir deg samtidig noen råd om prosess, verktøy og aktiviteter du kan benytte for å lykkes med å etablere en innovasjonskultur internt.',
+            'location' => 'Space 1 ',
+            'speaker' => 'Ragne Elisabeth Henriksen, Innovasjonsleder i Visma Consulting ',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'U7 - Kantega: Hvordan finne ut mer om brukerne dine enn brukeren selv vet',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:15',
+            'time_to' => '11:45',
+            'description' => 'Ingen diskripsjon',
+            'location' => 'Living Room 4',
+            'speaker' => 'Gøril Storrø',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'Sesjon 1A Kunsten å forenkle – suksesshistorier',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '12:00',
+            'time_to' => '13:00',
+            'description' => 'Det nye slagordet er «fornye, forenkle og forbedre». I denne sesjonen setter vi fokus på hvordan digitalisering bidrar til fornying, forenkling og forbedring. Vi får flere konkrete eksempler på hva virksomheter har gjort og hva som førte til deres suksess. Situasjonen før og etter tiltaket, beskrivelse av tiltaket og læringseffekten for andre vil være sentralt i de suksesshistoriene vi får høre om.',
+            'location' => 'Cosmos 1',
+            'speaker' => 'Hilde Kristin Storvig (Brønnøysundregistrene), Bjørn Sverre Gåsland (Statens Innkrevingssentral), Berner Koldal (Sjørfartsdirektoratet), Camilla Grøneng (Statens vegvesen), Torbjørn Moen (Oslo kemnerkontor).',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'Sesjon 1B Servicedesign - tjenesteutvikling med brukeren i sentrum',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '12:00',
+            'time_to' => '13:00',
+            'description' => 'Det duger ikke lenger å jobbe på «gamlemåten» når du skal utvikle dine tjenester for morgendagens brukere. Trender om forbruksmønstre og brukernes forventninger påvirker vår tjenesteutvikling. Servicedesign dreier seg om å skape gode og helhetlige opplevelser ved å sette brukeren i sentrum, i samsvar med virksomhetens strategi. I denne sesjonen setter vi søkelyset på servicedesign som fagdisiplin. Innovasjon knyttet til arbeidsprosesser og ivaretakelse av brukerbehov er noen stikkord ved tjenesteutvikling i en ny digital virkelighet. Er du og din virksomhet forberedt på fremtidens tjenesteutvikling? Hvis ikke - da må du få med deg denne sesjonen.',
+            'location' => 'Cosmos 3A',
+            'speaker' => 'Jørund Leknes (Kantega), Anne Stokke (Bouvet), Knut Kvale (Telenor ASA), Trude Hole (KulturIT).',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'Sesjon 1C Sikkerhet og beredskap',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '12:00',
+            'time_to' => '13:00',
+            'description' => 'Når vannet truer
+
+Geografisk informasjonsteknologi er samfunnet viktigste våpen for å verne liv og verdier når naturkreftene løper løpsk. Risiko- og sårbarhetsanalyser basert på denne teknologien gir planleggere og innbyggere langt større sikkerhet og forutsigbarhet enn tidligere. Foredraget viser en rekke praktiske situasjoner hvor moderne teknologi anvendes for å hindre eller redusere skade og fare.
+
+Én krise - flere kriseledelser – ett situasjonsbilde
+
+Effektiv krisehåndtering krever et samspill mellom flere aktører. Samfunnet disponerer samlet sett betydelige ressurser i form av nøyaktige geodata og spesialkompetanse. Utfordringen er at ressursene er fordelt på flere etater. Er aktørene i stand til, eller villige til å samhandle og ta i bruk ressursene på tvers av sektorer ? Foredraget viser hvordan og hvorfor en komponent for geobasert krisehåndtering er under oppbygging i Oslo kommune, Forsvaret og politiet.',
+            'location' => 'Cosmos 3C',
+            'speaker' => 'Erland Røed (Kartverket), Rune Bratlie (Nedre Eiker commune), Stein Moen (Fenrik, HV-01).',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'Lunsj',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '11:45',
+            'time_to' => '12:00',
+            'description' => 'Det blir servert lunsj i lunsjområdet.',
+            'location' => 'Lunsjområdet',
+            'speaker' => 'null',
+            'confirmed' => true,
+            'type' => 1,
+        ));
+
+        Program::create(array(
+            'title' => 'Paneldebatt: Ledere i utakt - manglende digital kompetanse?',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '14:00',
+            'time_to' => '15:00',
+            'description' => 'Vi trenger større fart på endringsarbeidet og mye kan gjøres i den enkelte enhet/etat.
+
+Hva kreves av ledere med ansvar for digitalisering?
+Hvilken kompetanse må de ha?
+Hvor står de i dag og hva skal til for å komme til ønsket tilstand?
+Kan digitalisering outsources til leverandører og konsulenter eller må virksomhetene selv stå for dette?
+Kan det tenkes at offentlig innkjøpsregime bidrar til å hindre utvikling av nødvendig kompetanse?',
+            'location' => 'Cosmos 1',
+            'speaker' => 'Arne Hjeltnes',
+            'confirmed' => true,
+            'type' => 0,
+        ));
+
+        Program::create(array(
+            'title' => 'Pause. Frukt, grønt og dip.',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '15:00',
+            'time_to' => '15:15',
+            'description' => 'Det blir servert en liten oppkvikker i fellesområdet.',
+            'location' => 'Fellesområdet',
+            'speaker' => 'null',
+            'confirmed' => true,
+            'type' => 1,
+        ));
+
+        Program::create(array(
+            'title' => 'Sesjon 2A Digital post',
+            'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+            'time_from' => '15:15',
+	        'time_to' => '16:15',
+	'description' => 'Vi har A-post og B-post, ja er det ikke noe som heter ePost og nå kommer D-post, nei det var digital post.
+    Vi produserer dokumenter digitalt, oppbevarer dokumenter digitalt, men når de skal sendes må de veien via papir –
+        dette gir vel ingen mening. Har du hørt om Difis prosjekt «Sikker digital post til innbygger» og KS KommITs «Svarut»,
+        post fra kommuner og fylkeskommuner til innbyggere og virksomheter. Digipost og eBoks har tilbudt digitale postkasser i
+        flere år allerede. Alle som ønsker det får selvangivelsen digitalt fra Altinn, men det er en digital meldingsboks,
+        ikke en digital postboks. Må vi beholde den grønne postkassen og hvorfor fjernet DNB muligheten kundene hadde til
+        å få kopi av posten i nettbanken sendt til eBoks og Digipost. Blir du med, vi samler alle til en prat, ja postbudet kommer kanskje også.',
+	'location' => 'Cosmos 3A',
+	'speaker' => 'Astrid Øksenvåg (eKor), Kjetil Århus (Bergen kommune), Tone Bringedal (DIFI), Kjersti Lauritzen (Brønnøysundregistrene).',
+	'confirmed' => true,
+	'type' => 0,
+));
+
+Program::create(array(
+	'title' => 'Sesjon 2B Brukersentrerte tjenester på tvers av etatenes siloer',
+	'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+	'time_from' => '15:15',
+	'time_to' => '16:15',
+	'description' => 'Mange offentlige tjenester lager gode digitale tjenester hver for seg. Men hva skal til for at vi får til samordnede tjenester som er sentrert rundt innbyggere og næringslivets behov og som involverer flere offentlige virksomheter?
+
+Etter en innledning om teamet får vi høre Jacob Krokstedt fra Stockholm stad som vil ha et innlegg med tittelen Med invånarnas fokus – Hur vi har jobbat med digitale tjänster på tvers av kommunala myndigheter i Stockholm. Til slutt blir det en paneldebatt med Arne Thorstensen fra Skatteetaten, Marit Mellingen, Difi og Arild Jansen, UiO.',
+	'location' => 'Cosmos 1',
+	'speaker' => 'Åsmund Mæhle (Bouvet), Jacob Krokstedt (Stockholm kommune), Arild Jansen (UiO), Arne Thorstensen (Skatteeetaten), Marit Mellingen (DIFI).',
+	'confirmed' => true,
+	'type' => 0,
+));
+
+Program::create(array(
+    'title' => 'Sesjon 2C Fra prosjekt til forvaltningv',
+    'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+    'time_from' => '15:15',
+	'time_to' => '16:15',
+	'description' => 'Overgangen fra prosjekt til forvaltning kan være stor. Hvordan kan prosjekter forberede seg på overgangen til forvaltning? Smidige metoder og å kombinere utvikling og forvaltning er tiltak som kan sikre en effektiv overgang fra prosjekt til forvaltning. I denne sesjonen vil vi fokusere på prosjekt og forvaltningsfase for Perform hos Statens Pensjonskasse, hvor utviklingsprosjektet varte fra 2008 til 2012 og systemet nå er i forvaltning. Vi vil fokusere på læringspunkter som er relevante både for små og store utviklingsprosjekt.',
+	'location' => 'Cosmos 3C',
+	'speaker' => 'Atle Bergfjord (Capgemini), Mette Gjertsen (Statens pensjonskasse), Tore Dybå (SINTEF)',
+	'confirmed' => true,
+	'type' => 0,
+));
+
+Program::create(array(
+	'title' => 'Konferansemiddag og Fyrlyktprisen',
+	'date' => DateTime::createFromFormat('d/m/Y', '29/10/2014'),
+	'time_from' => '19:30',
+	'time_to' => '23:30',
+	'description' => 'Mat og drikke er en viktig del av den totale opplevelsen av konferansen. Det blir det servert et velsmakende 3 retters måltid bestående av
+Forrett: Sukkersaltet Frøya Laks, Dillkrem, karse og marinert agurk
+Hovedrett: Langbakt Indrefilet av storfe, fenikkel, stekt sopp, pac choy, amadinepotet og timiansjy
+Dessert: Pasjonsfrukt panna cotta med mangokompott og sukkerbrød.',
+	'location' => 'Clarion Hotel & Congress',
+	'speaker' => 'null',
+	'confirmed' => true,
+	'type' => 1,
+));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 } 
