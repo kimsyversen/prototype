5e0c311766c6cb7fe02df689e3115be3029cdb21
Kim Syversen <kim.syversen@gmail.com>
2014-11-14 13:10:56 +0100

    More progress on program view

--- app/controllers/ProgramController.php
+++ app/controllers/ProgramController.php
@@ -26,18 +26,21 @@ class ProgramController extends BaseController{
         $items = DB::table('program')->select('program.pid as pid')->distinct()->get();
 
 
+        //For hvert item, trenger også time_from, time_to (det som skal stå i header for parallellsesjon
+
         foreach($items as $item)
         {
             $arr = DB::table('program')
                 ->select('program.id AS program_id', 'program.pid AS pid','program.title AS title', 'program.date AS date', 'program.time_from AS time_from',
                     'program.time_to AS time_to', 'program.description AS program_description',
-                    'program.location AS program_location, program.speaker AS program_speaker',
+                    'program.location AS program_location', 'program.speaker AS program_speaker',
                     'program.confirmed AS program_confirmed', 'program.type AS program_type')
                 ->where('program.pid', '=', $item->pid)
                 ->get();
 
 
-            array_push($items, $sessions);
+            $sessions[] = $arr;
+
         }
 
 
