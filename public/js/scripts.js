b4567617475ebf03b9860670e086bb982ef6c7a3
Kim Syversen <kim.syversen@gmail.com>
2014-11-18 12:50:26 +0100

    Forsker på sticky

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,3 +3,24 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
+
+/*$(document).ready(function() {
+    var stickyNavTop = $('.nav').offset().top;
+
+    var stickyNav = function(){
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > stickyNavTop) {
+            $('.nav').addClass('sticky');
+        } else {
+            $('.nav').removeClass('sticky');
+        }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function() {
+        stickyNav();
+    });
+});*/
